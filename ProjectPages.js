1248e99cc96d83c3d2407435619db5976da60fa3
Smos Bois <simondeb1505@gmail.com>
2023-06-09 18:09:13 +0200

    Made vertical transitions possible while using the auto height css thingy

--- ProjectPages.js
+++ ProjectPages.js
@@ -30,10 +30,6 @@ async function SpawnHorizontalPage(title, roles, description, images, link, butt
   //1: get the width of the popup
   //1.1: set the width of the popup based on the screen size
 
-  //for now we'll do it with a fixed width
-
-
-
   var mediaWidth = document.documentElement.clientWidth;
 
   //turns out a bunch of if-elses is the most efficient way to do this? I know, I don't like it either
@@ -143,19 +139,16 @@ async function SpawnVerticalPage(title, roles, description, images, link, button
   //1: get the width of the popup
   //1.1: set the width of the popup based on the screen size
 
-  //for now we'll do it with a fixed width
-
-
-
-  var mediaHeight = document.documentElement.clientHeight;
+  var mediaWidth = document.documentElement.clientWidth;
 
   //turns out a bunch of if-elses is the most efficient way to do this? I know, I don't like it either
-  if(mediaHeight >= 1580){ currentPopUp.style.height = "1400px"; } else
-  if(mediaHeight < 1580 && mediaHeight > 1360){ currentPopUp.style.height = "1200px";} else
-  if(mediaHeight < 1360 && mediaHeight > 1150){ currentPopUp.style.height = "1000px";} else
-  if(mediaHeight < 1150 && mediaHeight > 960){ currentPopUp.style.height = "800px";} else
-  if(mediaHeight < 960 && mediaHeight > 680){ currentPopUp.style.height = "600px";} else
-  if(mediaHeight < 680 || mediaHeight == null) { currentPopUp.style.height = "400px";}
+  if(mediaWidth >= 1580){ currentPopUp.style.width = "1400px"; } else
+  if(mediaWidth < 1580 && mediaWidth > 1360){ currentPopUp.style.width = "1200px";} else
+  if(mediaWidth < 1360 && mediaWidth > 1150){ currentPopUp.style.width = "1000px";} else
+  if(mediaWidth < 1150 && mediaWidth > 960){ currentPopUp.style.width = "800px";} else
+  if(mediaWidth < 960 && mediaWidth > 680){ currentPopUp.style.width = "600px";} else
+  if(mediaWidth < 680 || mediaWidth == null) { currentPopUp.style.width = "400px";}
+
 
   console.log("creating popUp");
 
@@ -235,8 +228,20 @@ async function SpawnVerticalPage(title, roles, description, images, link, button
   //5: set the height of the popup to 2x the image height 
   document.body.insertBefore(currentPopUp,document.getElementById("border"));
   await delay(10);
-  SetSizeInPixels(800, parseInt(currentPopUp.style.height));
+  SetSizeInPixels(parseInt(currentPopUp.style.width), 0);
   currentPopUp.style.height = "auto";
+
+  var newHeight = parseInt(currentPopUp.offsetHeight);
+
+  await delay(10);
+
+  SetSizeInPixels(parseInt(currentPopUp.style.width), 0);
+
+  await delay(10);
+
+  SetSizeInPixels(parseInt(currentPopUp.style.width), newHeight);
+  
+
   await delay(500);
   popUpImageParent.style.opacity = "1";
   popUpTextParent.style.opacity = "1";
